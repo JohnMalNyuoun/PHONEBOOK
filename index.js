dc174f564adff3b8122837775a6ef83de717d47c
Student <student@sirafrica.org>
2026-04-29 15:10:36 +0300

    feat: serve frontend build from backend for deployment

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'dist')))
 
 morgan.token('body', (req) => {
   if (req.method === 'POST') return JSON.stringify(req.body)
