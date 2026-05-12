a27b0b7dde89a01fed447789a9f6aec34f0a6180
Student <student@sirafrica.org>
2026-05-12 14:50:26 +0300

    Connect backend to MongoDB using Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./Modules/persons')
+
 const app = express()
 
 app.use(cors())
@@ -13,89 +16,106 @@ morgan.token('body', (req) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let phonebook = [
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3, name: 'Dan Abramov', number: '12-43-234345' }
-]
-
-const generateId = () =>
-  phonebook.length > 0 ? Math.max(...phonebook.map(p => p.id)) + 1 : 1
-
 app.get('/', (req, res) => {
-  res.send(`
-    <h1>Phonebook Backend</h1>
-    <p>Phonebook has info for ${phonebook.length} people</p>
-    <ul>
-      <li><a href="/api/persons">/api/persons</a></li>
-      <li><a href="/info">/info</a></li>
-    </ul>
-  `)
+  Person.find({}).then(persons => {
+    res.send(`
+      <h1>Phonebook Backend</h1>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <ul>
+        <li><a href="/api/persons">/api/persons</a></li>
+        <li><a href="/info">/info</a></li>
+      </ul>
+    `)
+  })
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = phonebook.find(p => p.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).json({ error: 'person not found' })
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).json({ error: 'person not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-  const time = new Date()
-  res.send(`
-    <p>Phonebook has info for ${phonebook.length} people</p>
-    <p>${time}</p>
-  `)
+  Person.find({}).then(persons => {
+    const time = new Date()
+    res.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${time}</p>
+    `)
+  })
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const { name, number } = req.body
 
   if (!name || !number) {
     return res.status(400).json({ error: 'name and number are required' })
   }
 
-  const existing = phonebook.find(p => p.name === name)
-  if (existing) {
-    return res.status(400).json({ error: 'name must be unique' })
-  }
+  const person = new Person({ name, number })
 
-  const newPerson = {
-    id: generateId(),
-    name,
-    number
-  }
-
-  phonebook = phonebook.concat(newPerson)
-  res.status(201).json(newPerson)
+  person.save()
+    .then(savedPerson => {
+      res.status(201).json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  phonebook = phonebook.filter(p => p.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const { number } = req.body
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
 
-  const index = phonebook.findIndex(p => p.id === id)
-  if (index === -1) {
-    return res.status(404).json({ error: 'person not found' })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        res.json(updatedPerson)
+      } else {
+        res.status(404).json({ error: 'person not found' })
+      }
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
-  phonebook[index] = { ...phonebook[index], number }
-  res.json(phonebook[index])
-})
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
